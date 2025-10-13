a1eb3b6ae18f99ca98ba5115050678dd6fdf4195
Soumya Banerjee <neel.soumya@gmail.com>
2025-10-13 17:24:13 +0100

    adding langchain examples wip

--- langchain_simple.py
+++ langchain_simple.py
@@ -3,6 +3,16 @@ Simple script to call LangChain
 '''
 
 import langchain
-#from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
+import dotenv
 
+# load environment variables from .env file
+dotenv.load_dotenv()
+
+# initialize the ChatOpenAI model
+llm = ChatOpenAI()
+
+# llm_response = llm.g
+
+#print(llm_response)
 
